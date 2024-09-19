4320acf3eeb40e3feb71d010d26d27524277ffc3
ElvisCayo <parapeggoelvis@gmail.com>
2024-09-19 14:46:32 -0300

    CINTA

--- pestaña de productos/Pasteleria/pasteleria.js
+++ pestaña de productos/Pasteleria/pasteleria.js
@@ -12,18 +12,36 @@ window.addEventListener('scroll', function() {
         barra.classList.remove("navBar");
         cintaIzquierda.classList.remove("categorias");
         cintaIzquierda.classList.add("categoriasFix");
-
+        cintaIzquierda.style.top="10%"
+        todosProductos.style.top="10%";
     }
     else{
         logo.style.display= "flex"
         barra.classList.add("navBar");
         barra.classList.remove("navBarFix");
-        todosProductos.style.top= "33%";;     
         cintaIzquierda.classList.remove("categoriasFix");
         cintaIzquierda.classList.add("categorias");
-
+        todosProductos.style.top="33%";
     }
 })
 function convertirVhAPixeles(vh) {
     return (window.innerHeight * vh) / 100;
 }
+
+const boton = document.querySelector("#boton");
+const todosProductos = document.querySelector(".todosProductos");
+activo= false;
+
+boton.addEventListener("click", () => {
+    
+
+    if (activo==false) {
+        todosProductos.style.visibility="visible"
+        todosProductos.style.position="fixed";
+
+    } else {
+        todosProductos.style.visibility="hidden"
+
+    }
+    activo = !activo; 
+})
