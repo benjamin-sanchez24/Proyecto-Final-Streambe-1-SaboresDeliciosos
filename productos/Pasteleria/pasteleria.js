46d6b4004cd7df34a0e6e99ca201cfc53e6af8c7
ElvisCayo <parapeggoelvis@gmail.com>
2024-09-30 09:39:45 -0300

    contenedor de productos

--- productos/Pasteleria/pasteleria.js
+++ productos/Pasteleria/pasteleria.js
@@ -2,8 +2,9 @@ let logo = document.querySelector(".home");
 let barra = document.querySelector(".navBar");
 let activar = convertirVhAPixeles(23);
 let cintaIzquierda = document.querySelector(".categorias");
+let contenido = document.querySelector(".ejemplares");
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', ()=> {
     let scroll = window.scrollY;
     if (scroll>activar){
         logo.style.display= "none"
@@ -12,6 +13,7 @@ window.addEventListener('scroll', function() {
         cintaIzquierda.classList.remove("categorias");
         cintaIzquierda.classList.add("categoriasFix");
         cintaIzquierda.style.top="10%"
+        contenido.style.marginTop="38vh"
     }
     else{
         logo.style.display= "flex"
@@ -19,8 +21,20 @@ window.addEventListener('scroll', function() {
         barra.classList.remove("navBarFix");
         cintaIzquierda.classList.remove("categoriasFix");
         cintaIzquierda.classList.add("categorias");
+        contenido.style.marginTop="5vh"
     }
 })
 function convertirVhAPixeles(vh) {
     return (window.innerHeight * vh) / 100;
 }
+
+
+botonInfo = document.querySelector(".ejemplar button")
+productoFoto = document.querySelector('.ejemplarImagen img').src;
+productoTitulo = document.querySelector('.ejemplarInfo h4').textContent;
+imagen = document.querySelector(".ponerImagen");
+titulo = document.getElementById('exampleModalLabel');
+botonInfo.addEventListener("click", ()=>{
+    imagen.src = productoFoto;
+    titulo.textContent = productoTitulo;
+})
