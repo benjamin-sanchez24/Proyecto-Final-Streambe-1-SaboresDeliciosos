862339a459863b43622cf3b12372195438a78784
Benjamin Sanchez <mariano.sanchez@santa-ana.edu.ar>
2024-10-01 20:47:19 -0300

    comit implementacion de base de datos

---
+++ config/bdsaboresdeliciosos.sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-10-2024 a las 01:46:44
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bdsaboresdeliciosos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `prodpanaderia`
+--
+
+CREATE TABLE `prodpanaderia` (
+  `id` int(11) NOT NULL,
+  `nombre` mediumtext NOT NULL,
+  `descripcion` longtext NOT NULL,
+  `precio` decimal(10,0) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  `activo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `prodpasteleria`
+--
+
+CREATE TABLE `prodpasteleria` (
+  `id` int(11) NOT NULL,
+  `nombre` mediumtext NOT NULL,
+  `descripcion` longtext NOT NULL,
+  `precio` decimal(10,0) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  `activo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `prodpanaderia`
+--
+ALTER TABLE `prodpanaderia`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `prodpasteleria`
+--
+ALTER TABLE `prodpasteleria`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `prodpanaderia`
+--
+ALTER TABLE `prodpanaderia`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `prodpasteleria`
+--
+ALTER TABLE `prodpasteleria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
